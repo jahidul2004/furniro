e6b35cebab2c3e82ba02fa73e6fc10c21de1c8b0
jahidul2004 <islamjahiduljihad@gmail.com>
2025-02-24 00:40:56 +0600

    Hero section added.

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from "./hero/Hero";
+
+const Home = () => {
+    return (
+        <>
+            {/* Hero */}
+            <Hero></Hero>
+            {/* Hero end */}
+        </>
+    );
+};
+
+export default Home;
---
+++ src/pages/home/hero/Hero.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import hero1 from "../../../assets/images/hero1.jpeg";
+const Hero = () => {
+    return (
+        <div
+            className="hero h-[500px]"
+            style={{
+                backgroundImage: `url(${hero1})`,
+            }}
+        >
+            <div className="hero-overlay opacity-10"></div>
+            <div className="hero-content text-neutral-content text-center grid grid-cols-1 md:grid-cols-2">
+                <div></div>
+                <div className="max-w-md bg-[#fff3e3] rounded text-left text-black p-4">
+                    <p className="text-lg">New Arrival</p>
+                    <h1 className="my-1 leading-12 text-3xl md:text-4xl font-bold text-[#ba8d2f]">
+                        Discover our new collection
+                    </h1>
+                    <p className="font-semibold text-sm text-[#4b4848]">
+                        Unveiling the latest addition to our Furion lineup!
+                        Explore a stunning range of cutting-edge designs,
+                        crafted with precision and innovation.
+                    </p>
+                    <Link className="cursor-pointer btn mt-2 bg-[#ba8d2f] text-white border-none shadow-none rounded-none px-5">
+                        Buy Now
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
