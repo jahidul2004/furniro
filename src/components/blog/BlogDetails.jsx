ec4e7a0e1d4ef2b91eb8ae955a5f9cb23c638270
jahidul2004 <islamjahiduljihad@gmail.com>
2025-04-12 12:50:18 +0600

    Blog details header area added.

--- src/components/blog/BlogDetails.jsx
+++ src/components/blog/BlogDetails.jsx
@@ -1,8 +1,23 @@
-
+import { FaGreaterThan } from "react-icons/fa";
+import shopHeading from "../../assets/pageHeading/shopHeading.png";
 const BlogDetails = () => {
     return (
         <div>
-            I am from blog details
+            {/* Header */}
+            <div
+                style={{
+                    backgroundImage: `url(${shopHeading})`,
+                    width: "100%",
+                }}
+                className="h-[220px] w-full bg-cover flex flex-col justify-center items-center"
+            >
+                <h1 className="text-3xl font-semibold">Blog Details</h1>
+                <p className="mt-2 font-semibold flex items-center">
+                    Home <FaGreaterThan className="mx-3" /> Blog{" "}
+                    <FaGreaterThan className="mx-3" /> Wooden Craft
+                </p>
+            </div>
+            {/* Header end */}
         </div>
     );
 };
