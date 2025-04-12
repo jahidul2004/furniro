290210f36bda8d0a4bc05c4f52344dc14ae7f43d
jahidul2004 <islamjahiduljihad@gmail.com>
2025-04-12 19:42:37 +0600

    Register page header section added.

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,5 +1,25 @@
+import { FaGreaterThan } from "react-icons/fa";
+import shopHeading from "../../assets/pageHeading/shopHeading.png";
+
 const Register = () => {
-    return <div>Hello I am Register</div>;
+    return (
+        <div>
+            {/* Header */}
+            <div
+                style={{
+                    backgroundImage: `url(${shopHeading})`,
+                    width: "100%",
+                }}
+                className="h-[220px] w-full bg-cover flex flex-col justify-center items-center"
+            >
+                <h1 className="text-3xl font-semibold">Register</h1>
+                <p className="mt-2 font-semibold flex items-center">
+                    Home <FaGreaterThan className="mx-3" /> Register
+                </p>
+            </div>
+            {/* Header end */}
+        </div>
+    );
 };
 
 export default Register;
