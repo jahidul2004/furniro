f19075a9500ab7c1c88d26e1afe4d0052c7b1c5f
jahidul2004 <islamjahiduljihad@gmail.com>
2025-04-12 10:57:29 +0600

    Contact page header area added.

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,23 @@
+import { FaGreaterThan } from "react-icons/fa";
+import shopHeading from "../../assets/pageHeading/shopHeading.png";
 
 const Contact = () => {
     return (
         <div>
-            I am from contact page
+            {/* Header */}
+            <div
+                style={{
+                    backgroundImage: `url(${shopHeading})`,
+                    width: "100%",
+                }}
+                className="h-[220px] w-full bg-cover flex flex-col justify-center items-center"
+            >
+                <h1 className="text-3xl font-semibold">Contact</h1>
+                <p className="mt-2 font-semibold flex items-center">
+                    Home <FaGreaterThan className="mx-3" /> Contact
+                </p>
+            </div>
+            {/* Header end */}
         </div>
     );
 };
