09e1de282aa1a865fb9a8682898efcdaa317a6bf
jahidul2004 <islamjahiduljihad@gmail.com>
2025-04-12 11:28:38 +0600

    About page header section added.

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,22 @@
+import { FaGreaterThan } from "react-icons/fa";
+import shopHeading from "../../assets/pageHeading/shopHeading.png";
 const About = () => {
     return (
-        <div className="w-[95%] md:container mx-auto my-10">
-            I am from about
+        <div>
+            {/* Header */}
+            <div
+                style={{
+                    backgroundImage: `url(${shopHeading})`,
+                    width: "100%",
+                }}
+                className="h-[220px] w-full bg-cover flex flex-col justify-center items-center"
+            >
+                <h1 className="text-3xl font-semibold">About</h1>
+                <p className="mt-2 font-semibold flex items-center">
+                    Home <FaGreaterThan className="mx-3" /> About
+                </p>
+            </div>
+            {/* Header end */}
         </div>
     );
 };
