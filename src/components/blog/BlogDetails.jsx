4d1ea9c8e608c52f5699f3bc3d5f554250455648
jahidul2004 <islamjahiduljihad@gmail.com>
2025-04-12 19:37:35 +0600

    Blog Details page and json file updated.

--- src/components/blog/BlogDetails.jsx
+++ src/components/blog/BlogDetails.jsx
@@ -1,6 +1,20 @@
-import { FaGreaterThan } from "react-icons/fa";
+import { FaGreaterThan, FaRegUser } from "react-icons/fa";
 import shopHeading from "../../assets/pageHeading/shopHeading.png";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { LuClock4 } from "react-icons/lu";
 const BlogDetails = () => {
+    const { id } = useParams();
+    const [blog, setBlog] = useState({});
+
+    useEffect(() => {
+        fetch("/blog.json")
+            .then((res) => res.json())
+            .then((data) => {
+                setBlog(data.find((item) => item.id == id));
+            });
+    }, []);
+
     return (
         <div>
             {/* Header */}
@@ -14,10 +28,32 @@ const BlogDetails = () => {
                 <h1 className="text-3xl font-semibold">Blog Details</h1>
                 <p className="mt-2 font-semibold flex items-center">
                     Home <FaGreaterThan className="mx-3" /> Blog{" "}
-                    <FaGreaterThan className="mx-3" /> Wooden Craft
+                    <FaGreaterThan className="mx-3" /> {blog?.title}
                 </p>
             </div>
             {/* Header end */}
+
+            {/* Details */}
+            <div className="w-[95%] md:container mx-auto my-10 grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
+                <div>
+                    <img
+                        className="w-full h-[400px] rounded"
+                        src={blog?.image}
+                        alt=""
+                    />
+                </div>
+
+                <div>
+                    <p className="text-gray-500 flex items-center gap-2 mb-2">
+                        <FaRegUser />
+                        {blog?.author} <LuClock4 />
+                        {blog?.date}
+                    </p>
+                    <h1 className="text-3xl font-bold mb-4">{blog?.title}</h1>
+                    <p>{blog?.content}</p>
+                </div>
+            </div>
+            {/* Details end */}
         </div>
     );
 };
