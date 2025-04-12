47bc65fbb2de52965c891d1417ee720bd91f46a8
jahidul2004 <islamjahiduljihad@gmail.com>
2025-04-12 11:49:07 +0600

    Blog section header area start.

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,8 +1,23 @@
+import { FaGreaterThan } from "react-icons/fa";
+import shopHeading from "../../assets/pageHeading/shopHeading.png";
 
 const Blog = () => {
     return (
         <div>
-            Hello I am Blog
+            {/* Header */}
+            <div
+                style={{
+                    backgroundImage: `url(${shopHeading})`,
+                    width: "100%",
+                }}
+                className="h-[220px] w-full bg-cover flex flex-col justify-center items-center"
+            >
+                <h1 className="text-3xl font-semibold">Blog</h1>
+                <p className="mt-2 font-semibold flex items-center">
+                    Home <FaGreaterThan className="mx-3" /> Blog
+                </p>
+            </div>
+            {/* Header end */}
         </div>
     );
 };
