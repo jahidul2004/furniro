f948cbb250c83cb6f313d0ae6aefeca72770ca04
jahidul2004 <islamjahiduljihad@gmail.com>
2025-04-25 21:35:35 +0600

    Product cart updated.

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 
 const ProductCard = ({ data }) => {
-    console.log(data);
     return (
         <div className="bg-[#f4f5f7] relative rounded">
             <div className="w-full h-[200px]">
@@ -21,14 +20,14 @@ const ProductCard = ({ data }) => {
                     <h3 className="font-semibold">$ {data?.price}</h3>
                     <Link
                         to={`/shop/${data?.id}`}
-                        className="rounded btn btn-sm mt-2 border-none shadow-none bg-[#2ec1ac] text-white"
+                        className="rounded btn btn-sm mt-2 border-none shadow-none bg-[#b98e2f] text-white"
                     >
                         View Details
                     </Link>
                 </div>
             </div>
             {data?.isNew && (
-                <div className="bg-[#2ec1ac] absolute top-2 right-2 p-2 w-[35px] h-[35px] flex justify-center items-center rounded-full">
+                <div className="bg-[#b98e2f] absolute top-2 right-2 p-2 w-[35px] h-[35px] flex justify-center items-center rounded-full">
                     <p className="text-white text-sm">New</p>
                 </div>
             )}
