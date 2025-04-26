037430e12e200311cc4da744fe334eea686072bc
jahidul2004 <islamjahiduljihad@gmail.com>
2025-04-26 17:17:05 +0600

    Users component completed.

--- src/pages/admin/users/Users.jsx
+++ src/pages/admin/users/Users.jsx
@@ -3,26 +3,26 @@ import { TbUsers } from "react-icons/tb";
 const Users = () => {
     return (
         <div className="p-6">
-            <div className="flex justify-between items-center mb-4">
+            <div className="flex flex-col md:flex-row justify-between items-center mb-4">
                 <h1 className="text-2xl font-bold flex items-center gap-2">
                     <TbUsers />
                     Users
                 </h1>
                 <input
-                    className="input"
+                    className="input mt-5 md:mt-0"
                     type="text"
-                    placeholder="Search User by Name"
+                    placeholder="Search User by Name or Email"
                 />
             </div>
             <div className="overflow-x-auto">
-                <table className="table">
+                <table className="table border border-gray-200">
                     {/* head */}
                     <thead>
                         <tr>
-                            <th></th>
+                            <th>SL</th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Email</th>
+                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -30,22 +30,34 @@ const Users = () => {
                         <tr className="bg-base-200">
                             <th>1</th>
                             <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
+                            <td>abc@def.com</td>
+                            <td>
+                                <button className="btn btn-xs btn-dash btn-error">
+                                    Remove
+                                </button>
+                            </td>
                         </tr>
                         {/* row 2 */}
                         <tr>
                             <th>2</th>
                             <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
+                            <td>abc@def.com</td>
+                            <td>
+                                <button className="btn btn-xs btn-dash btn-error">
+                                    Remove
+                                </button>
+                            </td>
                         </tr>
                         {/* row 3 */}
                         <tr>
                             <th>3</th>
                             <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
+                            <td>abc@def.com</td>
+                            <td>
+                                <button className="btn btn-xs btn-dash btn-error">
+                                    Remove
+                                </button>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
