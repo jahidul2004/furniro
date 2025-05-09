928f24c5ef5d9047728ff35a498879e71bc4209f
jahidul2004 <islamjahiduljihad@gmail.com>
2025-05-09 16:39:49 +0600

    Wishlist header area added.

--- src/pages/wishlist/WishList.jsx
+++ src/pages/wishlist/WishList.jsx
@@ -1,8 +1,22 @@
-
+import { FaGreaterThan } from "react-icons/fa";
+import shopHeading from "../../assets/pageHeading/shopHeading.png";
 const WishList = () => {
     return (
         <div>
-            I am wishList
+            {/* Header */}
+            <div
+                style={{
+                    backgroundImage: `url(${shopHeading})`,
+                    width: "100%",
+                }}
+                className="h-[220px] w-full bg-cover flex flex-col justify-center items-center"
+            >
+                <h1 className="text-3xl font-semibold">WishList</h1>
+                <p className="mt-2 font-semibold flex items-center">
+                    Home <FaGreaterThan className="mx-3" /> WishList
+                </p>
+            </div>
+            {/* Header end */}
         </div>
     );
 };
