ae4ecdd83afb6e6337c668bcd2289629118514d6
jahidul2004 <islamjahiduljihad@gmail.com>
2025-05-10 21:48:30 +0600

    sell details page updated admin.

--- src/pages/admin/sellDetails/SellDetails.jsx
+++ src/pages/admin/sellDetails/SellDetails.jsx
@@ -1,6 +1,6 @@
+import { useEffect, useState } from "react";
 import { BsClockHistory } from "react-icons/bs";
 import { CgSandClock } from "react-icons/cg";
-import { FaUsers } from "react-icons/fa6";
 import { HiOutlineViewGrid } from "react-icons/hi";
 import { IoArrowBackCircleOutline } from "react-icons/io5";
 import { MdOutlineCancelPresentation } from "react-icons/md";
@@ -9,15 +9,26 @@ import { VscError } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 
 const SellDetails = () => {
-    const sellData = {
-        totalSales: 150000,
-        cancelledOrdersAmount: 10000,
-        pendingOrdersAmount: 5000,
-        totalOrders: 120,
-        completedOrders: 100,
-        cancelledOrders: 10,
-        pendingOrders: 10,
-    };
+    const [sellData, setSellData] = useState([]);
+    const [pendingOrders, setPendingOrders] = useState(0);
+    const [completedOrders, setCompletedOrders] = useState(0);
+    const [cancelledOrders, setCancelledOrders] = useState(0);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/orderStats")
+            .then((res) => res.json())
+            .then((data) => {
+                data?.map((item) => {
+                    if (item._id === "pending") {
+                        setPendingOrders(item?.count);
+                    } else if (item._id === "completed") {
+                        setCompletedOrders(item?.count);
+                    } else {
+                        setCancelledOrders(item?.count);
+                    }
+                });
+            });
+    }, []);
 
     return (
         <div className="p-6 bg-gray-100 min-h-screen">
@@ -31,29 +42,19 @@ const SellDetails = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-center">
                 <div className="bg-white p-6 rounded-lg shadow-md">
-                    <TbCoinTaka
-                        size={32}
-                        className="mx-auto mb-2 text-success"
-                    />
-
                     <p className="text-2xl font-bold text-success">
                         {sellData?.totalSales} BDT
                     </p>
                     <h2 className="text-xl font-semibold mb-4">Total Sales</h2>
                     <Link
                         to={"/admin/dashboard/completedOrders"}
-                        className="btn btn-sm border border-success border-dashed text-success"
+                        className="btn btn-sm btn-success btn-soft"
                     >
                         View
                     </Link>
                 </div>
 
                 <div className="bg-white p-6 rounded-lg shadow-md">
-                    <MdOutlineCancelPresentation
-                        size={32}
-                        className="mx-auto mb-2 text-error"
-                    />
-
                     <p className="text-2xl font-bold text-error">
                         {sellData?.cancelledOrdersAmount} BDT
                     </p>
@@ -62,15 +63,13 @@ const SellDetails = () => {
                     </h2>
                     <Link
                         to={"/admin/dashboard/completedOrders"}
-                        className="btn btn-sm border border-error border-dashed text-error"
+                        className="btn btn-sm btn-error btn-soft"
                     >
                         View
                     </Link>
                 </div>
 
                 <div className="bg-white p-6 rounded-lg shadow-md">
-                    <CgSandClock size={32} className="mx-auto mb-2 text-info" />
-
                     <p className="text-2xl font-bold text-info">
                         {sellData?.cancelledOrdersAmount} BDT
                     </p>
@@ -79,61 +78,52 @@ const SellDetails = () => {
                     </h2>
                     <Link
                         to={"/admin/dashboard/completedOrders"}
-                        className="btn btn-sm border border-info border-dashed text-info"
+                        className="btn btn-sm btn-info btn-soft"
                     >
                         View
                     </Link>
                 </div>
 
                 <div className="bg-white p-6 rounded-lg shadow-md">
-                    <HiOutlineViewGrid
-                        size={32}
-                        className="mx-auto mb-2 text-success"
-                    />
-
                     <p className="text-2xl font-bold text-success">
-                        {sellData?.cancelledOrdersAmount} BDT
+                        {completedOrders}
                     </p>
-                    <h2 className="text-xl font-semibold mb-4">Total Orders</h2>
+                    <h2 className="text-xl font-semibold mb-4">
+                        Completed Orders
+                    </h2>
                     <Link
                         to={"/admin/dashboard/completedOrders"}
-                        className="btn btn-sm border border-success border-dashed text-success"
+                        className="btn btn-sm btn-success btn-soft"
                     >
                         View
                     </Link>
                 </div>
 
                 <div className="bg-white p-6 rounded-lg shadow-md">
-                    <VscError size={32} className="mx-auto mb-2 text-error" />
                     <p className="text-2xl font-bold text-error">
-                        {sellData?.cancelledOrdersAmount} BDT
+                        {cancelledOrders}
                     </p>
                     <h2 className="text-xl font-semibold mb-4">
                         Cancelled Orders
                     </h2>
                     <Link
                         to={"/admin/dashboard/completedOrders"}
-                        className="btn btn-sm border border-error border-dashed text-error"
+                        className="btn btn-sm btn-error btn-soft"
                     >
                         View
                     </Link>
                 </div>
 
                 <div className="bg-white p-6 rounded-lg shadow-md">
-                    <BsClockHistory
-                        size={32}
-                        className="mx-auto mb-2 text-info"
-                    />
-
                     <p className="text-2xl font-bold text-info">
-                        {sellData?.cancelledOrdersAmount} BDT
+                        {pendingOrders}
                     </p>
                     <h2 className="text-xl font-semibold mb-4">
                         Pending Orders
                     </h2>
                     <Link
                         to={"/admin/dashboard/completedOrders"}
-                        className="btn btn-sm border border-info border-dashed text-info"
+                        className="btn btn-sm btn-info btn-soft"
                     >
                         View
                     </Link>
